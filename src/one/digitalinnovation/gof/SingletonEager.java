bd122b66e1828f993d33ecf1229fc7d57d96ecf7
Matheus De Alencar <mathstrax@gmail.com>
2022-01-28 16:01:19 -0300

    Singleton

---
+++ src/one/digitalinnovation/gof/SingletonEager.java
@@ -0,0 +1,21 @@
+package one.digitalinnovation.gof;
+
+/**
+ * Singleton "apressado"
+ *
+ *  ele instancia direto, enquanto o preguiçoso faz a verificação
+ *
+ * @author  Mathstrax
+ */
+
+public class SingletonEager {
+
+    private static final SingletonEager instancia =  new SingletonEager();
+
+    private  SingletonEager() {
+        super();
+    }
+    public static SingletonEager getInstancia() {
+        return instancia;
+    }
+}
---
+++ src/one/digitalinnovation/gof/SingletonLazy.java
@@ -0,0 +1,29 @@
+package one.digitalinnovation.gof;
+
+/**
+ * Singleton "Preguiçoso".
+ *
+ * Singleton preguiçoso ele não disponibiliza a instância paro usuario.
+ *
+ * @author Mathstrax
+ *
+ *
+ *
+ */
+
+public class SingletonLazy {
+    private static SingletonLazy instancia;
+
+    private SingletonLazy() {
+        super();
+    }
+
+    public static SingletonLazy getInstancia() {
+        if (instancia == null) {
+            instancia = new SingletonLazy();
+
+        }
+        return instancia;
+    }
+
+}
